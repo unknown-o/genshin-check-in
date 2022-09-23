05d6f801dd032cbfa209b71bb80f3c03d53f9f50
UnknownO <i@mr-wu.top>
2022-09-23 19:21:39 +0800

    feat: 修改python脚本签到返回文本

--- python/index.py
+++ python/index.py
@@ -56,13 +56,17 @@ def check_in(cookie_string):
     cookies = cookie_str2dict(cookie_string)
     game_info = get_game_info(cookies)
     if (game_info['retcode'] != 0):
-        print("签到失败！")
+        print("Failed to get game info")
         print(game_info)
     else:
-        print("执行成功！")
-        print(game_info)
-        print(bbs_sign_reward(cookies, get_ds(), game_info))
-
+        print("Successfully obtained game information")
+        # print(game_info)
+        check_in_res = bbs_sign_reward(cookies, get_ds(), game_info)
+        if(check_in_res['retcode'] == 0):
+            print("Sign in successfully")
+        else:
+            print("Failed to sign in")
+            print(check_in_res)
 
 # python index.py "_MHYUUID=bd20878a-1d35-457c-b84c-9ea929ced396; _ga=GA1.2.128251121.1653128272; _gid=GA1.2.321587272.1653128272; _gat=1"
 check_in(sys.argv[1])
